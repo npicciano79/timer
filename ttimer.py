30d0d0655ecd9314ba0e7ec191657f44b30381d9
npicciano79 <npicciano79@gmail.com>
2021-10-25 18:47:21 -0400

    ttimer.py

--- ttimer.py
+++ ttimer.py
@@ -10,7 +10,7 @@ background2='#000000'
 
 #create 2 frames
 start_frame=tk.Frame(r,bg=background)
-time_frame=tk.Frame(r,bg=background2)
+time_frame=tk.Frame(r,bg=background)
 
 
 #button to enter time and switch frames
@@ -22,9 +22,17 @@ s_button.place(relx=0.4,rely=0.7,relheight=0.2,relwidth=0.2)
 t_button=tk.Button(time_frame,text="Stop",command=lambda:start())             
 t_button.place(relx=0.4,rely=0.7,relheight=0.2,relwidth=0.2)
 
+#user enters time in textbox
+s_text=tk.Text(start_frame,height=5, width=25,font=text_font)
+s_text.place(relx=0.35,rely=0.1,relwidth=0.3,relheight=0.2)
+
+#directions label
+s_title=tk.Label(start_frame,height=7,width=50,text="Enter your time and press Start",bg=background)
+s_title.place(relx=0.35,rely=0.5,relwidth=0.3,relheight=0.2)
+
 
 def timer():
-    time_frame.place(relx=0.05,rely=0.05,relwidth=0.9,relheight=0.5)
+    time_frame.place(relx=0.05,rely=0.05,relwidth=0.9,relheight=0.9)
     start_frame.place_forget()
 
 
