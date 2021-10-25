203f9769486cf7520c6c3d583195d724cd3351dd
npicciano79 <npicciano79@gmail.com>
2021-10-25 18:00:31 -0400

    timer.py

--- timer.py
+++ timer.py
@@ -4,10 +4,13 @@ import tkinter as tk
 
 def input():
     inputVal=t_text.get("1.0","end-1c")
+    timer(inputVal)
+
+
+
+
+def timer(inputVal):
     print(inputVal)
-
-
-
 
 
 
@@ -22,10 +25,9 @@ t_text=tk.Text(s_frame,height=5, width=25,font=text_font)
 t_text.place(relx=0.35,rely=0.1,relwidth=0.3,relheight=0.2)
 s_button=tk.Button(s_frame,text="Start",command=lambda:input())
 s_button.place(relx=0.4,rely=0.5,relheight=0.2,relwidth=0.2)
+t_frame=tk.Frame(r,bg="#000000")
+t_frame.place(relx=0.05,rely=0.05,relwidth=0.9,relheight=0.9)
 
-
-def timer(test):
-    print(test)
 
 
 if __name__=="__main__":
