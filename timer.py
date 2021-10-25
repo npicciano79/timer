a30345bc81030798a38b459cfa4212703eaf3151
npicciano79 <npicciano79@gmail.com>
2021-10-25 13:05:41 -0400

    timer.py

--- timer.py
+++ timer.py
@@ -18,9 +18,12 @@ s_frame=tk.Frame(r,bg=background)
 s_frame.place(relx=0.05,rely=0.05,relwidth=0.9,relheight=0.9)
 t_text=tk.Text(s_frame,height=5, width=25,font=text_font)
 t_text.place(relx=0.35,rely=0.1,relwidth=0.3,relheight=0.2)
-s_button=tk.Button(s_frame,text="Start")
+s_button=tk.Button(s_frame,text="Start",command=lambda:timer(t_text.get))
 s_button.place(relx=0.4,rely=0.5,relheight=0.2,relwidth=0.2)
 
+
+def timer(test):
+    print(test)
 
 
 if __name__=="__main__":
