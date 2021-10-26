c30e1835ced235e0196d357ce1fdb1b6256e79b7
npicciano79 <npicciano79@gmail.com>
2021-10-25 20:52:56 -0400

    ttimer.py

--- ttimer.py
+++ ttimer.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from typing import Text
 
 #create tk instance 
 r=tk.Tk()
@@ -31,15 +32,36 @@ s_title=tk.Label(start_frame,text="Enter your time and press Start",bg=backgroun
 s_title.place(relx=0.25,rely=0.5,relwidth=0.5,relheight=0.2)
 
 
+
+
+#timer frame
 def timer():
+    inputVal=int(s_text.get("1.0","end-1c"))
+    #print(inputVal)
     time_frame.place(relx=0.05,rely=0.05,relwidth=0.9,relheight=0.9)
     start_frame.place_forget()
-
-
-def start():
+    #countdown label found on timer frame
+    countdown=tk.Label(time_frame,text=inputVal)
+    countdown.place(relx=0.35,rely=0.1,relwidth=0.3,relheight=0.2)
+    countdown(inputVal,countdown)
+ 
+#starting frame
+def start(): 
     start_frame.place(relx=0.05,rely=0.05,relwidth=0.9,relheight=0.9)
     time_frame.place_forget()
 
+
+
+#countdown timer
+def countdown(inputVal,countdown):
+    print(inputVal)
+    #timer countdown, appear on time_frame, called by timer
+    if inputVal > 0:
+        inputVal-=1
+        countdown.set(inputVal)
+        r.after(1000,lambda:countdown(inputVal,countdown))
+    else:
+        close()
 
 
 if __name__=="__main__":
